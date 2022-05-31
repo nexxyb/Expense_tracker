d292ed974f33fdb03e0b80855f2bd7135e975d30
nexxyb <nexxyb@gmail.com>
2022-05-31 17:01:59 +0100

    Update expense tracker.py

--- expense tracker.py
+++ expense tracker.py
@@ -2,11 +2,17 @@
 from datetime import datetime as dt
 expense_list= {}
 
-expense = str(input("Enter Expense Type: "))
-amount= int(input ("Enter Expense Amount: "))
-date= dt.now().strftime("%Y-%m-%d")
-f_expense= {expense: [amount,date]}
-expense_list.update(f_expense)
+while True:
+    expense = input(str("Enter Expense Type: "))
+    if expense != 'done':
+        amount= int(input("Enter Expense Amount: "))
+        date= dt.now().strftime("%Y-%m-%d")
+        f_expense= {expense: [amount,date]}
+        expense_list.update(f_expense)
+    elif expense == 'done':
+        break
+    
+    
 
 #display input
 print(expense_list)
